e021cfbedbc3a053e8d9be4372edd09776546f76
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-21 12:03:12 -0500

    feat: :sparkles: add filter by day

--- etl/trsf/transform.py
+++ etl/trsf/transform.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import Any
 
 
 def pdjson_normalize(*, df: pd.DataFrame, orient='records', sep='_') -> pd.DataFrame:
@@ -21,4 +22,51 @@ def pdjson_normalize(*, df: pd.DataFrame, orient='records', sep='_') -> pd.DataF
     """
     df_norm: pd.DataFrame = pd.json_normalize(df.to_dict(orient=orient), sep=sep)
     return df_norm
+
+
+def filter_by_day(*, df: pd.DataFrame, date_col: str, start_date: str, end_date: str) -> pd.DataFrame:
+    """
+    Filter a dataframe by date.
+
+    Parameters
+    ----------
+    df
+        The dataframe to filter.
+    date_col
+        The column in the dataframe to filter by.
+    start_date
+        The start date to filter by.
+    end_date
+        The end date to filter by.
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe with the filtered data.
+    """
+    df_filtered: pd.DataFrame = df[
+        (df[date_col] >= start_date) &
+        (df[date_col] <= end_date)
+    ]
+    return df_filtered
+
+
+def get_max_data_column(*, df: pd.DataFrame, column: str) -> Any:
+    """
+    Get the maximum value from a column in a dataframe.
+
+    Parameters
+    ----------
+    df
+        The dataframe to get the column from.
+    column
+        The column to get.
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe with the column.
+    """
+    max_value: Any = df[column].max()
+    return max_value
 
