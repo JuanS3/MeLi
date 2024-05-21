e7b0807841f425977f4bcf46850b69dfe8c53fb9
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-20 23:45:55 -0500

    feat: :sparkles: add method to save parquet file

---
+++ etl/load/load.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from etl.utils import decorators as dec
+
+
+@dec.time_it
+def dataframe_to_parquet(
+        *,
+        df: pd.DataFrame,
+        path: str = 'data/staging/dataframe.parquet.gzip',
+        compression: str = 'gzip',
+    ) -> None:
+    """
+    Save a pandas DataFrame to Parquet format.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to save.
+    path : str | Optional
+        The path where the DataFrame should be saved, by default 'data/staging/dataframe.parquet.gzip'
+    compression : str | Optional
+        The compression mode to use for the Parquet file, by default 'gzip'
+    """
+    df.to_parquet(path, compression=compression, engine='pyarrow')
