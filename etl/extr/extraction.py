30d74a2bf8e5ebd030e1b94038fcd05e1f839737
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-20 21:49:10 -0500

    feat: :sparkles: add methods for load csv and json files

---
+++ etl/extr/extraction.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+def load_csv(*, file_path: str, delimeter: str = ',') -> pd.DataFrame:
+    """
+    Reads a csv file and returns a pandas dataframe
+
+    Parameters
+    ----------
+    file_path: str
+        The path of the file
+    delimeter: str
+        The delimeter of the file
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas dataframe with the data from the csv file
+    """
+    df: pd.DataFrame = pd.read_csv(file_path, delimiter=delimeter)
+    return df
+
+
+def load_json(*, file_path: str, multi_json: bool = False) -> pd.DataFrame:
+    """
+    Reads a json file and returns a pandas dataframe
+
+    Parameters
+    ----------
+    file_path: str
+        The path of the file
+    multi_json: bool
+        If the file has multiple json objects
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas dataframe with the data from the json file
+    """
+    df: pd.DataFrame = pd.read_json(file_path) if not multi_json else pd.read_json(file_path, lines=True)
+    return df
+
